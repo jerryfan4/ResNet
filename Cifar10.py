37684cc6b632b2d3e01336e86f74906a26844d87
jerryfan4 <jerry.fan1996@gmail.com>
2017-08-02 15:06:00 -0700

    Add Cifar10 dataset model

---
+++ Cifar10.py
@@ -0,0 +1,94 @@
+import urllib
+import os
+import tarfile
+import cPickle
+import numpy as np
+
+def maybe_download(target_dir):
+    cifar_data_url = 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
+    cifar_data_file = os.path.join(target_dir, 'cifar-10-python.tar.gz')
+    if not os.path.exists(cifar_data_file):
+        print 'Downloading cifar dataset...'
+        urllib.urlretrieve(cifar_data_url, cifar_data_file)
+    
+        print 'Extracting cifar dataset...'
+        tar = tarfile.open(cifar_data_file, "r:gz")
+        tar.extractall(cifar_dir)
+        tar.close()    
+
+class Cifar10(object):
+    def __init__(self, train_batch_size, test_batch_size):
+        self.train_batch_size = train_batch_size
+        self.test_batch_size = test_batch_size
+
+        download_dir = './data'
+        if not os.path.exists(download_dir):
+            os.mkdir(download_dir)
+        maybe_download(download_dir)
+        
+        self.cifar_dir = os.path.join(download_dir, 'cifar-10-batches-py')
+        self.train_images, self.train_labels = load_train_data()
+        self.test_images, self.test_labels = load_test_data()
+        self.pp_mean = get_per_pixel_mean()
+        self.normalize()
+        self.shuffle()
+
+        self.train_batch_count = self.train_images.shape[0] // self.train_batch_size
+        self.test_batch_count = self.test_images.shape[0] // self.test_batch_size
+
+    def load_train_data():
+        train_files = ['data_batch_1',
+                       'data_batch_2',
+                       'data_batch_3',
+                       'data_batch_4',
+                       'data_batch_5']
+        images, labels = [], []
+
+        for data_file in train_files:
+            full_path = os.path.join(self.cifar_dir, data_file)
+            with open(full_path, 'rb') as f:
+                raw = cPickle.load(f)
+
+            count = raw['data'].shape[0]
+            batch = np.transpose(raw['data'].reshape((count, 32, 32, 3)), (0, 2, 3, 1))
+
+            images += list(batch)
+            labels += raw['labels']
+        return np.array(images).astype(np.float32), np.array(labels)
+
+    def load_test_data():
+        test_file = 'test_batch'
+        images, labels = [], []
+
+        full_path = os.path.join(self.cifar_dir, test_file)
+        with open(full_path, 'rb') as f:
+            raw = cPickle.load(f)
+    
+        count = raw['data'].shape[0]
+        batch = np.transpose(raw['data'].reshape((count, 3, 32, 32)), (0, 2, 3, 1))
+    
+        images += (list(batch))
+        labels += raw['labels']  
+        return np.array(images).astype(np.float32), np.array(labels)
+
+    def get_per_pixel_mean():
+        images = np.concatenate((self.train_images, self.test_images), axis=0)
+        return np.mean(images, axis=0)
+
+    def shuffle(self):
+        self.shuffle = np.random.permutation(self.train_images.shape[0])
+
+    def normalize(self):
+        self.train_images = (self.train_images - self.pp_mean) / 128.0
+        self.test_images = (self.test_images - self.pp_mean) / 128.0
+
+    def next_train_batch(self, idx):
+        batch_images = self.train_images[shuffle[idx * self.train_batch_size : (idx + 1) * self.train_batch_size]]
+        batch_labels = self.train_labels[shuffle[idx * self.train_batch_size : (idx + 1) * self.train_batch_size]]
+
+    def next_test_batch(self, idx):
+        batch_images = self.test_images[idx * self.test_batch_size : (idx + 1) * self.test_batch_size]
+        batch_labels = self.test_labels[idx * self.test_batch_size : (idx + 1) * self.test_batch_size]
+
+
+
